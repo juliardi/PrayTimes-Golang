1bf585c6042951b8123c120bbbc164968314e0b6
Juliardi <ardi93@gmail.com>
2017-09-06 14:58:37 +0700

    fix Adjust function behavior

--- PrayTimes_test.go
+++ PrayTimes_test.go
@@ -1,26 +1,46 @@
 package praytimes
 
 import (
-    "testing"
-    "fmt"
-    "time"
-    "strings"
+	"fmt"
+	"strings"
+	"testing"
+	"time"
 )
 
 func TestGetTimes(t *testing.T) {
-    times := GetTimes(time.Now(), []float64 {43, -80}, -5, 0, "")
+	times := GetTimes(time.Now(), []float64{43, -80}, -5, 0, "")
 
-    fmt.Println("Prayer Times for today in Waterloo/Canada\n")
-    for _, val := range []string {"Fajr", "Sunrise", "Dhuhr", "Asr", "Maghrib", "Isha", "Midnight"} {
-        fmt.Printf("%v: %v\n", val, times[strings.ToLower(val)])
-    }
+	fmt.Println("Prayer Times for today in Waterloo/Canada")
+	for _, val := range []string{"Fajr", "Sunrise", "Dhuhr", "Asr", "Maghrib", "Isha", "Midnight"} {
+		fmt.Printf("%v: %v\n", val, times[strings.ToLower(val)])
+	}
 
-    fmt.Println()
-    times = GetTimes(time.Now(), []float64{ -6.9034443, 107.5731164 }, 7)
-    fmt.Println("Prayer Times for today in Bandung/Indonesia\n")
-    for _, val := range []string {"Fajr", "Sunrise", "Dhuhr", "Asr", "Maghrib", "Isha", "Midnight"} {
-        fmt.Printf("%v: %v\n", val, times[strings.ToLower(val)])
-    }
+	fmt.Println()
+	times = GetTimes(time.Now(), []float64{-6.9034443, 107.5731164}, 7)
+	fmt.Println("Prayer Times for today in Bandung/Indonesia")
+	for _, val := range []string{"Fajr", "Sunrise", "Dhuhr", "Asr", "Maghrib", "Isha", "Midnight"} {
+		fmt.Printf("%v: %v\n", val, times[strings.ToLower(val)])
+	}
 
-    fmt.Println()
+	fmt.Println()
+}
+
+func TestAdjust(t *testing.T) {
+	setting := GetSetting()
+
+	fmt.Println("Setting for", GetMethod(), "method")
+	for _, val := range []string{"highLats", "midnight", "imsak", "fajr", "dhuhr", "asr", "maghrib", "isha"} {
+		fmt.Printf("%v: %v | ", val, setting[val])
+	}
+
+	fmt.Print("\n")
+
+	SetMethod("Egypt")
+	setting = GetSetting()
+	fmt.Println("Setting for", GetMethod(), "method")
+
+	for _, val := range []string{"highLats", "midnight", "imsak", "fajr", "dhuhr", "asr", "maghrib", "isha"} {
+		fmt.Printf("%v: %v | ", val, setting[val])
+	}
+	fmt.Print("\n")
 }
